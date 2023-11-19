a235e03f8d37109f8b67f8cefc0836ea8ba4f83e
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-11-19 19:28:27 +0530

    bug fix

--- src/utils/3_Constants.ts
+++ src/utils/3_Constants.ts
@@ -10,5 +10,5 @@ const MONGODB_USER = "tom"
 const MONGODB_PASSWORD = "waterbottle"
 export const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.yyiz64c.mongodb.net/?retryWrites=true&w=majority`;
 
-export const SOCKET_SERVER_URL = "http://streaming-server-waterbottle.koyeb.app/"
+export const SOCKET_SERVER_URL = "https://streaming-server-waterbottle.koyeb.app/"
 // export const SOCKET_SERVER_URL = "http://localhost:8000"
